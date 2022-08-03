cde6c5840f59a18135facbae72757d0d257b753b
AlexeyEf <avefimov2013@gmail.com>
2022-08-04 01:42:27 +0300

    modified 11.6.4 task code

--- C++/11/task_4/main.cpp
+++ C++/11/task_4/main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include <vector>
 
-struct StrState
-{
-    bool valid = true;
-    std::string winner;
-};
-
-StrState checkWinner(const std::vector<std::string> strings);
-StrState checkStrings(const std::vector<std::string> strings);
-StrState checkOneString(const std::string str);
-std::vector<std::string> getColumns(std::vector<std::string> strings);
-std::vector<std::string> getDiags(std::vector<std::string> strings);
+//уместно ли использовать в качестве возвр. значений char и вектора из char
+char checkGame(const std::vector<std::string> strings);
+std::vector<char> checkStrings(const std::vector<std::string> strings);
+std::vector<char> checkOneString(const std::string str);
+bool checkCorrectInputString(const std::string inputStr);
+std::vector<std::string> getColumns(const std::vector<std::string> strings);
+std::vector<std::string> getDiags(const std::vector<std::string> strings);
 
 
 int main()
@@ -26,16 +22,36 @@ int main()
     std::cin >> str2;
     std::cin >> str3;
 
+    //правильно и корректно ли применять такую конструкцию ниже (3 функции подряд в условии цикла)?
+    while(!(checkCorrectInputString(str1) && checkCorrectInputString(str2) && checkCorrectInputString(str3)))
+    {
+        std::cout << "Invalid input" << std::endl;
+        std::cout << "Input tic-tac-toe strings sequentially: " << std::endl;
+        std::cin >> str1;
+        std::cin >> str2;
+        std::cin >> str3;
+    }
+
     strings.push_back(str1);
     strings.push_back(str2);
     strings.push_back(str3);
 
-    StrState gameWinner= checkWinner(strings);
-    if(gameWinner.valid)
+    char gameWinner = checkGame(strings);
+
+    if(gameWinner == 'X')
     {
-        std::cout << gameWinner.winner << " won" << std::endl;
+        std::cout << "Petya won" << std::endl;
     }
-    else
+    else if(gameWinner == 'O')
+    {
+        std::cout << "Vanya won" << std::endl;
+    }
+    else if(gameWinner == '.')
+    {
+        std::cout << "Nobody" << std::endl;
+    }
+        //корректно ли сообщать об ошибке в вызывающую функцию с помощью -1?
+    else if(gameWinner == -1)
     {
         std::cout << "Incorrect" << std::endl;
     }
@@ -43,47 +59,133 @@ int main()
     return 0;
 }
 
-StrState checkWinner(const std::vector<std::string> strings)
+bool checkCorrectInputString(const std::string inputStr)
 {
-    StrState result;
-
-    if(strings.empty())
+    if(inputStr.empty() || inputStr.length() != 3)
     {
-        result.valid = false;
-        return result;
+        return false;
     }
 
-    StrState resultRow;
-    StrState resultColumn;
-    StrState resultDiag;
-
-    resultRow = checkStrings(strings);
-    resultColumn = checkStrings(getColumns(strings));
-    resultDiag = checkStrings(getDiags(strings));
-
-    result.valid = resultRow.valid && resultColumn.valid && resultDiag.valid &&
-                   (!resultRow.winner.empty() || !resultColumn.winner.empty() ||
-                   !resultDiag.winner.empty());
-
-    //проверка если два победителя
-
-    if(!resultRow.winner.empty())
+    for(int i = 0; i < inputStr.length(); ++i)
     {
-        result.winner = resultRow.winner;
+        if((inputStr[i] != 'X') && (inputStr[i] != 'O') && (inputStr[i] != '.'))
+        {
+            return false;
+        }
     }
-    else if(!resultColumn.winner.empty())
-    {
-        result.winner = resultColumn.winner;
-    }
-    else if(!resultDiag.winner.empty())
-    {
-        result.winner = resultDiag.winner;
-    }
+    return true;
+}
 
+char checkGame(const std::vector<std::string> strings)
+{
+    //целесообразно ли здесь объявлять 3 переменные?
+    std::vector<char> result1 = {0, 0, 0};
+    std::vector<char> result2 = {0, 0, 0};
+    std::vector<char> result3 = {0, 0, 0};
+
+    result1 = checkStrings(strings);
+    result2 = checkStrings(getColumns(strings));
+    result3 = checkStrings(getDiags(strings));
+
+    //уместна ли здесь такая громоздкая конструкция?
+    if(result1[2] == -1 || result2[2] == -1 || result3[2] == -1      ||
+       result1[2] != '.' &&  result2[2] != '.' && result3[2] != '.'  &&
+       (result1[2] == result2[2] || result2[2] == result3[2] || result1[2] == result3[2]))
+    {
+        return -1;
+    }
+    else if(result1[2] == '.' && result2[2] == '.' && result3[2] == '.')
+    {
+        return '.';
+    }
+    else if(result1[2] == 'X' || result2[2] == 'X' || result3[2] == 'X')
+    {
+        if((result1[0] - result1[1]) != 1)
+        {
+            return -1;
+        }
+        else
+        {
+            return 'X';
+        }
+    }
+    else if(result1[2] == 'O' || result2[2] == 'O' || result3[2] == 'O')
+    {
+        if((result1[1] - result1[0]) != 1)
+        {
+            return -1;
+        }
+        else
+        {
+            return 'O';
+        }
+    }
+}
+
+std::vector<char> checkStrings(const std::vector<std::string> strings)
+{
+    bool winner = false;
+    std::vector<char> resultFunc = {0, 0, 0}; //возвращение результата данной функции в формате
+    //{сколько X, сколько O, победитель}
+    std::vector<char> result = {0, 0}; //возвращение результата функции проверки одной строки
+    //выдает количество знаков X или O в строке
+
+    for(int i = 0; i < strings.size(); ++i)
+    {
+        result = checkOneString(strings[i]);
+
+        for(int j = 0; j < result.size(); ++j)
+        {
+            resultFunc[j] += result[j];
+        }
+
+        if((result.at(0) == 3) || (result.at(1) == 3))
+        {
+            if(winner)
+            {
+                resultFunc[2] = -1;
+                return resultFunc;
+            }
+            else
+            {
+                winner = true;
+                if(result.at(0) == 3)
+                {
+                    resultFunc[2] = 'X';
+                }
+                else if((result.at(1) == 3))
+                {
+                    resultFunc[2] = 'O';
+                }
+            }
+        }
+    }
+    if(!winner)
+    {
+        resultFunc[2] = '.';
+    }
+    return resultFunc;
+}
+
+std::vector<char> checkOneString(const std::string str)
+{
+    std::vector<char> result = {0, 0};
+
+    for(int i = 0; i < str.length(); ++i)
+    {
+        if(str[i] == 'X')
+        {
+            ++result[0];
+        }
+        else if(str[i] == 'O')
+        {
+            ++result[1];
+        }
+    }
     return result;
 }
 
-std::vector<std::string> getColumns(std::vector<std::string> strings)
+std::vector<std::string> getColumns(const std::vector<std::string> strings)
 {
     std::vector<std::string> result;
     std::string column;
@@ -100,7 +202,7 @@ std::vector<std::string> getColumns(std::vector<std::string> strings)
     return result;
 }
 
-std::vector<std::string> getDiags(std::vector<std::string> strings)
+std::vector<std::string> getDiags(const std::vector<std::string> strings)
 {
     std::vector<std::string> result;
     std::string diag1;
@@ -109,80 +211,10 @@ std::vector<std::string> getDiags(std::vector<std::string> strings)
     for(int i = 0, j = 0; i < strings.size() && j < strings.front().length(); ++i, ++j)
     {
         diag1.push_back(strings[i][j]);
-        diag2.push_back(strings[2 - i][2 - j]);
+        diag2.push_back(strings[2 - i][j]);
     }
     result.push_back(diag1);
     result.push_back(diag2);
 
-    return result;
-}
-
-StrState checkOneString(const std::string str)
-{
-    StrState result;
-
-    if(str.empty() || str.length() > 3)
-    {
-        result.valid = false;
-        return result;
-    }
-
-    int countX = 0, countO = 0, countDot = 0;
-
-    for(int i = 0; i < str.length(); ++i)
-    {
-        if(str[i] == 'X')
-        {
-            ++countX;
-        }
-        else if(str[i] == 'O')
-        {
-            ++countO;
-        }
-        else if(str[i] == '.')
-        {
-            ++countDot;
-        }
-    }
-    if(countX == 3)
-    {
-        result.winner = "Petya";
-    }
-    else if(countO == 3)
-    {
-        result.winner = "Vanya";
-    }
-    else if(countDot == 3)
-    {
-        result.winner = "Nobody";
-    }
-    else if((countX + countO + countDot) != 3)
-    {
-        result.valid = false;
-    }
-
-    return result;
-}
-
-StrState checkStrings(const std::vector<std::string> strings)
-{
-    StrState result;
-    bool winner = false;
-
-    for(int i = 0; i < strings.size(); ++i)
-    {
-        result = checkOneString(strings[i]);
-        if(!result.valid)
-        {
-            break;
-        }
-        if(!result.winner.empty()) {
-            if (winner) {
-                result.valid = false;
-                break;
-            }
-            winner = true;
-        }
-    }
     return result;
 }
