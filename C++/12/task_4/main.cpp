cc4d605bcd4f1a0761147f264b0278f0e9b0819d
AlexeyEf <avefimov2013@gmail.com>
2022-10-03 23:29:36 +0300

    modified 12.5.4 task code

--- C++/12/task_4/main.cpp
+++ C++/12/task_4/main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 
-//Для сокращения используемой памяти
-//использовал типы переменных
-//в зависимости от возможных
-//принимаемых значений этих переменных.
-//Вместо int использовал тип int8_t,
-//т.к. возможные значения этих переменных малы.
-//Стоит ли вообще так поступать?
-//В программистской практике так делают?
-
 int main()
 {
-    const int8_t size = 15;
-    int x;
+    const int size = 15;
+    int a[15];
 
-    std::cout << " Input initial number X: ";
-    std::cin >> x;
+    std::cout << " Input 15 consecutive positive integers, two of which are repeated: ";
 
-    int a[size] = {x+1, x+6, x+9, x+2, x, x+3, x+10, x+5,
-                   x+3, x+4, x+13, x+11, x+8, x+12, x+7};
+    for(int i = 0; i < size; ++i)
+    {
+        std::cin >> a[i];
+    }
 
+    int x = a[0] - 15;//принимаю за минимальное значение
     int repeatNumb = 0;
-    int8_t counters[size] = {0};
+    int8_t counters[2*size] = {0};//счетчик разброса значений
 
-    for(int8_t i = 0; i < size; ++i)
+    for(int i = 0; i < size; ++i)
     {
         if(++counters[a[i] - x] > 1)
         {
