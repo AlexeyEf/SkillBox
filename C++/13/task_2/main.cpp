77800afca35f8b80f773d76d1af6121e7aae2aad
AlexeyEf <avefimov2013@gmail.com>
2022-10-04 23:39:34 +0300

    added 13.6.2 task code

---
+++ C++/13/task_2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<float> prices = {15.75, 17.2, 5.8, 9.2, 10.7};
+    std::vector<int> items = {2, 4, 0, 1, 3};
+    int sum = 0;
+
+    for(int i = 0; i < prices.size(); ++i)
+    {
+       sum += prices[i]*items[i];
+    }
+
+    std::cout << "Cost of purchases = " << sum << std::endl;
+
+    return 0;
+}
