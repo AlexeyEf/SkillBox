3e8029e157b74c0ff671087114bdd29cda84f830
AlexeyEf <avefimov2013@gmail.com>
2023-01-02 00:30:57 +0300

    fix bugs in 14.6.7 task code

--- C++/14/task_7/main.cpp
+++ C++/14/task_7/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 
-bool initWorld(int world[][5][10]);
-bool inputWorld(int world[][5][10]);
-bool showHorizLevel(int level, int world[][5][10]);
+bool inputWorld(bool world[][5][10]);
+bool showHorizLevel(int level, bool world[][5][10]);
 
 int main()
 {
-    int world[5][5][10];
+    bool world[5][5][10] = {false};
     int level = 0;
-
-    initWorld(world);//инициализация трехмерного массива нулями
-                     //int world[5][5][10] = {0}; не срабатывает - вопрос почему?
 
     std::cout << "Input height of each world point (from 1 to 10): " << std::endl;
     inputWorld(world);
@@ -27,22 +23,7 @@ int main()
     return 0;
 }
 
-bool initWorld(int world[][5][10])
-{
-    for(int i = 0; i < 5; ++i)
-    {
-        for(int j = 0; j < 5; ++j)
-        {
-            for(int k = 0; k < 10; ++k)
-            {
-                world[i][j][k] = 0;
-            }
-        }
-    }
-    return true;
-}
-
-bool inputWorld(int world[][5][10])
+bool inputWorld(bool world[][5][10])
 {
     for(int i = 0; i < 5; ++i)
     {
@@ -51,14 +32,14 @@ bool inputWorld(int world[][5][10])
             std::cin >> input;
             for(int k = 0; k < input; ++k)
             {
-                world[i][j][k] = 1;
+                world[i][j][k] = true;
             }
         }
     }
     return true;
 }
 
-bool showHorizLevel(int level, int world[][5][10])
+bool showHorizLevel(int level, bool world[][5][10])
 {
     std::cout << "World slice: " << level << std::endl;
 
